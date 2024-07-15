7c7810b0f95a3f3f39e281bb59fee651ec095193
ishatarpada <ishatarpada@gmail.com>
2024-07-15 12:58:12 +0530

    Added

--- resourse_controller/routes/web.php
+++ resourse_controller/routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
 });
+ */
+
+/* Route::resource('users', UserController::class); */
+
+/* Route::resource('users', UserController::class)->only('create', 'update', 'show'); */
+
+/* Route::resource('users', UserController::class)->except('create', 'update', 'show'); */
+
+/* Route::resource('users', UserController::class)->names([
+    'create' => 'users.build',
+    'show' => "users.view"
+]); */
+
+Route::resource('users', UserController::class);
+Route::resource('users.comments' , CommentController::class)->shallow();
