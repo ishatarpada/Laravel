a3c723d30201cc36e194922844cd06106f0c6979
ishatarpada <ishatarpada@gmail.com>
2024-07-15 10:51:33 +0530

    Added

--- request-validation/app/Http/Controllers/UserController.php
+++ request-validation/app/Http/Controllers/UserController.php
@@ -2,19 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function addUser(Request $req){
-        $req->validate([
-            'name' => 'required',
-            'city' => 'required',
-            'age' => 'required|numeric|min:18',
-            'email' => 'required|email',
-            'password' => 'required|alpha_num|min:6'
-        ]);
+    public function addUser(UserRequest $req){
+        // $req->validate([
+        //     'name' => 'required',
+        //     'city' => 'required',
+        //     'age' => 'required|numeric|min:18',
+        //     'email' => 'required|email',
+        //     'password' => 'required|alpha_num|min:6'
+        // ]);
 
-        return $req->all();
+        // return $req->all();
+        // return $req->only(['name' , 'city']);
+        // return $req->except(['password' , 'city']);
     }
+
+
+    
 }
--- request-validation/app/Http/Requests/UserRequest.php
+++ request-validation/app/Http/Requests/UserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
 
 class UserRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,44 @@ class UserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+
+            'name' => 'required',
+            'city' => 'required',
+            'age' => 'required|numeric|min:18',
+            'email' => 'required|email',
+            'password' => 'required|alpha_num|min:6'
         ];
     }
+
+    // public function messages()
+    // {
+    //     return [
+    //         "name.required" => ':attribute is Required !',
+    //         "email.required" => 'email is Required !',
+    //         "age.required" => 'age is Required !',
+    //         "password.required" => 'password is Required !',
+    //         "city.required" => 'city is Required !'
+    //     ];
+    // }
+
+    public function attributes()
+    {
+        return [
+            'name' => 'Name',
+            'city' => 'City',
+            'age' => 'Age',
+            'email' => 'Email',
+            'password' => 'Password'
+        ];
+    }
+
+    // protected function prepareForValidation(): void
+    // {
+    //     $this->merge([
+    //         'name' => strtoupper($this->name),
+    //         'name' => str::slug($this->name)
+    //     ]);
+    // }
+
+    protected $stopOnFirstFailure = true;
 }
